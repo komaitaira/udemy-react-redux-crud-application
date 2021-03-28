13681febfd05d7bc9e4fdfd80f85a0f0dab16332
komaitaira <komaitaira@gmail.com>
2021-03-29 00:32:13 +0900

    boilerplate作成および不要ファイル削除

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>hello world</p>
-      </header>
+    <div>
+      <h1>hello, world</h1>
     </div>
   );
 }
