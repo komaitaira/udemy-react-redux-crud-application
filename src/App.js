51824124c52598a5f99f22ef0929f1cea5d6c2f6
komaitaira <komaitaira@gmail.com>
2021-04-02 22:55:55 +0900

    Fix propsTypes 型定義

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, {Component} from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName", age: 3 }
+  ]
   return (
     <div>
-      <h1>hello, world</h1>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
-  );
+  )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and { props.age } years old.</div>
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
