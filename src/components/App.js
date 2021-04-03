95c54dc55309d3043a7af33c762a644cf1d39757
komaitaira <komaitaira@gmail.com>
2021-04-03 10:54:28 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "NoName", age: 3 }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )
+import { increment, decrement } from '../actions'
+
+class App extends Component {
+  render() {
+    const props = this.props;
+    return (
+      <React.Fragment>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and { props.age } years old.</div>
-}
+const mapStateToProps = state => ({ value: state.count.value })
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App)
